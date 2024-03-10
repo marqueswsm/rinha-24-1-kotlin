fc2f216bfeeb946b3eac228ebfc7293ac22b3ba4
marqueswsm <wagner_marques@live.com>
2024-03-10 15:26:04 -0300

    first commit

---
+++ src/main/kotlin/com/marqueswsm/rinha/RinhaApplication.kt
@@ -0,0 +1,11 @@
+package com.marqueswsm.rinha
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RinhaApplication
+
+fun main(args: Array<String>) {
+	runApplication<RinhaApplication>(*args)
+}
---
+++ src/test/kotlin/com/marqueswsm/rinha/RinhaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.marqueswsm.rinha
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RinhaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
